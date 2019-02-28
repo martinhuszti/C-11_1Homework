d70bac3168150511c918289dd10ff408506dc61c
Martin <martinhuszti@gmail.com>
2019-02-28 19:32:36 +0100

    commit #2

--- mystring.h
+++ mystring.h
@@ -1,7 +1,17 @@
 #ifndef MYSTRING_H
 #define MYSTRING_H
+#include <iostream>
 
 class MyString {
+public:
+MyString();
+MyString(const char *);
+MyString(MyString const &m);
+
+void operator=(const MyString);
+void operator=(const char*);
+MyString& operator[](int m);
+MyString operator[](int m) const;
 
 };
 
