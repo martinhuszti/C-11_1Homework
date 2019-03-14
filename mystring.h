1cad92f4a6b3fed6fa7ca6ade1f6396b678db743
Martin <martinhuszti@gmail.com>
2019-03-14 23:52:29 +0100

    += =

--- mystring.h
+++ mystring.h
@@ -16,39 +16,121 @@ StringValue(const char * s) : size{strlen(s)}, ref_counter{1}{
 }
 StringValue() : size{0},ref_counter{1}{
 								str = new char[1];
-								str[0] = 0;//'\0'
+								str[0] = 0;  //'\0'
 }
 ~StringValue(){
 								delete[] str;
 								ref_counter=0;
 								size =0;
 }
-char *writeString() const {
+
+char *getStr() const {
 								return str;
+}
+size_t getSize() const {
+								return size;
+}
+int getRef_count() const {
+								return ref_counter;
+}
+void incRef(){
+								ref_counter++;
+}
+void decRef(){
+								ref_counter--;
 }
 };
 
-//void operator=(){}
+
 
 class MyString {
 
 private:
 StringValue * strvalue;
 public:
-const char * writeString() const {
-								return strvalue->writeString();
+
+
+
+//kiíráshoz
+const char * getString() const {
+								if(strvalue!=nullptr)
+																return strvalue->getStr();
 }
+//üres konstruktor //új karaktertömb kell
 MyString() : strvalue{new StringValue}{
 }
+//értékadó konstruktor //új karaktertömb kell
 MyString(const char * chars){
 								strvalue = new StringValue{chars};
+}
+//másoló konstruktor //refeneciaszám növelése
+MyString(const MyString &otherms){
+								this->strvalue = otherms.strvalue;
+								strvalue->incRef();
+}
+//destruktor
+~MyString(){
+								this->decRef();
+}
+
+size_t size() const
+{
+								return strvalue->getSize();
+}
+
+void decRef(){
+								if(strvalue==nullptr) return;
+								strvalue->decRef();
+								if(strvalue->getRef_count() == 0) {
+																delete strvalue;
+																strvalue = nullptr;
+								}
+}
+
+
+char& operator[](int index){
+								if(strvalue->getRef_count()<=1) {
+																return strvalue->getStr()[index];
+								}
+								auto tempStr = getString();
+								decRef();
+								strvalue = new StringValue{tempStr};
+								return strvalue->getStr()[index];
+}
+
+MyString& operator=(const MyString& ms){
+								strvalue = ms.strvalue;
+								strvalue->incRef();
+								return *this;
+}
+
+MyString& operator+=(const MyString& other){
+								char temp[size()+other.size()+1];
+								strcpy (temp,getString() );
+								strcat(temp,other.getString());
+								decRef();
+								strvalue = new StringValue{temp};
+
+								return *this;
+
+
+}
+
+MyString operator+(const MyString& other){
+								MyString temp{*this};
+								temp+=other.strvalue->getStr();
+								return temp;
 }
 
 };
 
+
+
 std::ostream& operator<<(std::ostream& os, const MyString s){
-								os << s.writeString();
+								os << s.getString();
 								return os;
 }
+
+
 
 #endif
--- test.cpp
+++ test.cpp
@@ -1,13 +1,39 @@
 #include "mystring.h"
 #include <iostream>
 
-int main(int argc, char const *argv[]) {
-        MyString ures; std::cout << "Üres String létrehozva" << std::endl;
-        MyString inicalizalt("init me pls"); std::cout << "Inicalizalt String létrehozva" << std::endl;
-        std::cout<<inicalizalt <<std::endl << "-----------------------------";
+int szamlalo= 1;
 
-        MyString x = "hello";
-        MyString y = x;
-        std::cout << y;
+void testBegin(const MyString& s){
+        std::cout<<">>" << szamlalo <<". teszteset: ";
+        std::cout << s <<"<<" << std::endl;
+}
+void testEnd(){
+        std::cout << std::endl<<szamlalo++ <<". teszt vége" << std::endl
+                  << "----------------------------" << std::endl<<std::endl;
+}
+int main(int argc, char const *argv[]) {
+        testBegin("String Létrehozása üresen");
+        MyString ures;
+        std::cout<< ures;
+        testEnd();
+
+        testBegin("String létrehozása karaktertömbből");
+        MyString inited("Én vagyok az inicializat String");
+        std::cout << inited;
+        testEnd();
+
+        testBegin("Két String összefűzése +-al");
+        MyString egy("egy+++"); MyString ketto("+++ketto");
+        MyString osszefuzve = egy + ketto;
+        std::cout <<osszefuzve;
+        testEnd();
+
+        testBegin("Két String összefűzése +=-vel");
+        egy="egy+++"; ketto="===ketto";
+        osszefuzve = egy + ketto;
+        std::cout <<osszefuzve;
+        testEnd();
+
+        std::cout << "\n\nTeszteknek vége";
 
 }
